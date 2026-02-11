df834d31d2a7f8eae56bdf713fafed003552f5ef
kumarchandan125 <chandanpandey6048178@gmail.com>
2026-02-11 22:05:26 +0530

    install all file related to typescript and this is my day 1

---
+++ DAY-1/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person},welcom to chaicode!`;
+}
+const username = 'chai aur typescript';
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ DAY-1/src/index.ts
@@ -0,0 +1,6 @@
+function greet(person:string):string{
+    return `Hello ${person},welcom to chaicode!`
+}
+
+const username:string='chai aur typescript';
+console.log(greet(username))
